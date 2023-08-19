5fd4de6d7e85907369797329d44f270dcea3743d
wendel filipe <wendelfilipe1997@hotmail.com>
2023-08-18 23:47:18 -0300

    Padrao

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,18 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Declarando variáveis numericas");
-Console.WriteLine();
-
-// declarando variaveis
-byte valor1 = 255;
-sbyte valor2 = -127;
-int valor3 = -2147483647;
-uint valor4 = 2147483647;
-long valor5 = -2147483647;
-
-Console.WriteLine(valor1);
-Console.WriteLine(valor2);
-Console.WriteLine(valor3);
-Console.WriteLine(valor4);
-
-Console.ReadLine();
+Console.WriteLine("");
