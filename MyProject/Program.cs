3791a939d7901288b0415d5a0d15cc0c5574bfd2
wendel filipe <wendelfilipe1997@hotmail.com>
2023-08-19 00:00:29 -0300

    bool e char

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("Atribuido valores para o bool e char");
+
+bool ativo = true;
+Boolean inativo = false;
+
+Console.WriteLine(ativo);
+Console.WriteLine(inativo);
+
+Console.WriteLine(10 == 15);
+Console.WriteLine(10 == 10);
+
+int x = 10;
+int y = 15;
+Console.WriteLine(x > y);
+
+char letra1 = 'A';
+char letra2 = '\u0041';
+
+Console.WriteLine(letra1);
+Console.WriteLine(letra2);
+
+Console.ReadLine();
