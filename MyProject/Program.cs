47cdc3f4c861e8ffc5d91ca7287f780aa7ee7356
wendel filipe <wendelfilipe1997@hotmail.com>
2023-08-20 16:31:00 -0300

    reference type

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("Definindo variaevis do tipo string e object e dynamic");
+Console.WriteLine();
+
+string nome = "wendel";
+String titulo = "Curso de C#";
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+
+string valor = "string";
+
+valor = "alterada";
+
+//StringBuilder
+
+object nota = 10;
+object valor1 = 8.55m;
+
+Console.WriteLine(nota);
+Console.WriteLine(valor1);
+Console.WriteLine(valor);
+
+Console.ReadLine();
