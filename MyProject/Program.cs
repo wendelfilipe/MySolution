82fd044fa9de489a6820ac381d60b266c3b8c7b7
wendel filipe <wendelfilipe1997@hotmail.com>
2023-08-21 20:18:36 -0300

    doing lessons

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("Exercícios");
+
+// 1- Crie uma variável chamada idade e atribua a ela o valor 35 e exiba o seu valor.
+
+int age = 35;
+
+Console.WriteLine(age); 
+
+//2- Crie uma variável chamada nome e atribua o valor "Maria" e exiba o seu valor.
+
+string name = "Maria";
+
+Console.WriteLine(name);
+
+//3- Crie uma variável chamada altura e atribua a ela o valor 3.45.
+
+float high = 3.45f;
+
+Console.WriteLine(high);
+
+//4- Crie uma variável chamada data e atribua a ela a data 04/09/1999 e exiba o seu valor.
+
+DateTime date = new DateTime(1999,09,04);
+
+Console.WriteLine(date);
+
+//5- Crie uma constante chamada ano e atribua a ela o valor 12 e exiba o seu valor;
+
+const int ano = 12;
+
+Console.WriteLine(ano);
+
+// 6- Declare uma variável nota do tipo double como sendo do tipo Nullable type e atribua o valor 7.80 e exiba o seu valor.
+
+double? nota = 7.80;
+
+Console.WriteLine(nota);
+
+// Declare duas variáveis x e y como sendo do tipo int e atribua a ela os valores 77 e 66 e a
+// seguir imprima o valor da soma de x com y. (Use o operador aritmético + para realizar a
+// operação de soma)
+
+int x = 77;
+int y = 66;
+int z = x + y;
+
+Console.WriteLine(z);
+
