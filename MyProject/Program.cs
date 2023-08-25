857d9985d0d2eeaa410d92c9a5a20aa1687c9b5a
wendel filipe <wendelfilipe1997@hotmail.com>
2023-08-24 21:45:13 -0300

    learning escape sequence

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("Saida de dados : Usando sequencia de escapes\n");
+
+string local = "c:\\dados\\poesias.txt";
+
+string frase = "Ele falou: \"Não fui eu\"";
+
+string pizza = "\nPizza\nde\nMussarela";
+
+string bolo = "Bolo\tde\tChocolate";
+
+Console.WriteLine(pizza);
+Console.WriteLine(bolo);
+
+Console.ReadLine();
