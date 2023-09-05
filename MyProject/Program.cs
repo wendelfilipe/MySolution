228f852b5f1d6ade8b186e9b6a7c8f95fe080a96
wendel filipe <wendelfilipe1997@hotmail.com>
2023-09-05 00:03:55 -0300

    learning operator precendence and associativity

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("Precendência e associatividade de operadores");
+
+int x = (10 - 2) * 3;
+
+Console.WriteLine(x);
+
+bool y = !(9 != 8) && (5 >= 7 || 8 >= 6);
+
+Console.WriteLine(y);
+
+int a = 5, b = 6, c = 4;
+
+int r = --a * b - ++c;
+
+Console.WriteLine(r);
