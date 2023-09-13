d3ebebc19e7ffdbed2d0b0360f32092c9033f521
wendel filipe <wendelfilipe1997@hotmail.com>
2023-09-12 22:50:50 -0300

    learning ternary-operator

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("Operadores uniarioss, binários e operador condicional ternário");
+
+int positivo = 1;
+int resultado;
+
+resultado = +positivo;
+Console.WriteLine(resultado);
+
+Console.WriteLine("informe o numero: ");
+var n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"O negativo de {n} é {-n}");
+
+Console.WriteLine("operador condicional ternário");
+
+Console.WriteLine("Informe a temperatura: \n");
+var temp = Convert.ToDouble(Console.ReadLine());
+
+var resultadoTemp = temp > 27 ? "Quente" : "frio";
+
+Console.WriteLine(resultadoTemp);
+
+Console.WriteLine("aninhando operações");
+
+Console.WriteLine("informe o valor de x :");
+var x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("informe o valor de y :");
+var y = Convert.ToInt32(Console.ReadLine());
+
+var resultado2 = x > y ? $"{x} é maior que {y}" :
+    x < y ? $"{x} é menor que {y}" :
+x == y ? $"{x} é igual a {y}" : "Sem resultado";
+Console.WriteLine(resultado2);
+        
+Console.ReadKey();
