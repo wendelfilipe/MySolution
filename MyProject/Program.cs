92f4b8e5410516b924ac5ea90d2f534da2bd628c
wendel filipe <wendelfilipe1997@hotmail.com>
2023-09-20 20:18:06 -0300

    learnng if instruction

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+
+Console.WriteLine("Instrução if");
+
+Console.WriteLine("Cliente é especial y/n");
+string response = Console.ReadLine();
+
+if (response == "s")
+{
+    Console.WriteLine("Cliente tem 10% de desconto");
+}
+if (response != "y" || response != "n")
+{
+    Console.WriteLine("Responda com (y) para sim, e (n) para não");
+}
