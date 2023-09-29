4cb8816ecc1661eccadeef66af8149e9b1d3cb19
wendel filipe <wendelfilipe1997@hotmail.com>
2023-09-29 08:00:38 -0300

    learning loop for

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("loop for");
+
+Console.WriteLine("Digite um numero de 1 aé 10");
+int y = Convert.ToInt32(Console.ReadLine());
+while (y < 0 || y > 10)
+{
+    Console.WriteLine("digite um numero de 1 ate 10");
+    y = Convert.ToInt32(Console.ReadLine());
+}
+for (int x = 1; x <= 10; x++)
+{
+    Console.WriteLine($"a tabuada de {y} x {x} = {y * x}");
+}
