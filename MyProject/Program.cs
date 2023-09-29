f82a70897d0adf0ae5011e3404a083d976ba3045
wendel filipe <wendelfilipe1997@hotmail.com>
2023-09-29 07:28:45 -0300

    learning loop while

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("while aninhado");
+
+int x = 0;
+while (x > 5)
+{
+    int y = 0;
+    while (y > 5)
+    {
+        Console.WriteLine($"{x}, {y}");
+        y++;
+    }
+    x++;
+    Console.WriteLine();
+}
