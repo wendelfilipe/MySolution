7b2015468aa7ef879526982716c267d8931420e6
wendel filipe <wendelfilipe1997@hotmail.com>
2023-10-04 20:56:46 -0300

    learning methods with parameters

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("metodos com paramentros");
+
+// var x = new Parameters();
+//
+// x.name = "wendel";
+// x.date = DateTime.Now;
+//
+// var name = x.name;
+// var date = x.date;
+//
+// Parameters.Saudacao(name, date);
+//
+// class Parameters
+// {
+//     public string? name;
+//     public DateTime date;
+//
+//     public static void Saudacao(string name, DateTime date)
+//     {
+//         Console.WriteLine(name);
+//         Console.WriteLine(string.Format(date.ToString()));
+//     }
+// }
+
+var minhaClasse = new MinhaClasse();
+
+minhaClasse.Saudacao("Maria", DateTime.Now.ToShortDateString());
+
+public class MinhaClasse
+{
+    public void Saudacao(string? name, string? date)
+    {
+        Console.WriteLine(name);
+        Console.WriteLine(date);
+    }
+}
