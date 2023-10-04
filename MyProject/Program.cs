543ed7964716f4873a41dc264f40664c88ab4bb7
wendel filipe <wendelfilipe1997@hotmail.com>
2023-10-04 08:00:10 -0300

    learning methods

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("Metodos");
+
+
+
+var minhaClasse = new MinhaClasse();
+
+minhaClasse.Saudacao();
+//minhaClasse.ExibirDataAtual();
+
+class MinhaClasse
+{
+    public void Saudacao()
+    {
+        Console.WriteLine("Bem vindo ");
+        Console.WriteLine("15/10/2022");
+        ExibirDataAtual();
+    }
+
+    public void ExibirDataAtual()
+    {
+        Console.WriteLine(DateTime.Now.ToShortDateString());
+    }
+}
