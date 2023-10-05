939f293ab368da1ae2b2cf9ba396c0d6c9489015
wendel filipe <wendelfilipe1997@hotmail.com>
2023-10-05 07:55:42 -0300

    learning more than one constructors

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("Mais de um construtor");
+
+var aluno = new Aluno("Maria", 22, "feminino", "S");
+
+Console.WriteLine(aluno.Name == null ? "null" : aluno.Name);
+Console.WriteLine(aluno.Idade == null ? "null" : aluno.Idade);
+Console.WriteLine(aluno.Sexo == null ? "null" : aluno.Sexo);
+Console.WriteLine(aluno.Aprovado == null ? "null" : aluno.Aprovado);
+
+public class Aluno
+{
+    public Aluno(string name) => Name = name;
+
+    public Aluno(string name, int idade, string sexo, string aprovado) : this(name)
+    {
+        Idade = idade;
+        Sexo = sexo;
+        Aprovado = aprovado;
+    }
+    
+    
+    public string? Name;
+    public int Idade;
+    public string? Sexo;
+    public string? Aprovado;
+}
