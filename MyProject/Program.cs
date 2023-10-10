16b07f2137efe055618d44e7b9ccfb91cc01d42e
wendel filipe <wendelfilipe1997@hotmail.com>
2023-10-10 19:55:52 -0300

    doing exercices 4

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("Exercicos modulo methods");
+
+var chevrolet = new Carro("Sedan", "Chevrolet", "Onix", 2016, 110);
+
+var ford = new Carro("SUV", "Ford", "EcoSport", 2018, 120);
+
+Console.WriteLine($"os valores são {chevrolet.modelo} {chevrolet.montadora} {chevrolet.marca} {chevrolet.ano} {chevrolet.potencia}");
+chevrolet.Acelerar(chevrolet.marca);
+Console.WriteLine($"os valores são {ford.modelo} {ford.montadora} {ford.marca} {ford.ano} {ford.potencia}");
+ford.Acelerar(ford.marca);
+public class Carro
+{
+    public string modelo;
+    public string montadora;
+    public string marca;
+    public int ano;
+    public double potencia;
+
+    public void Acelerar(string marca)
+    {
+        Console.WriteLine($"Acelerando o meu {marca}");
+    }
+
+    public Carro(string modelo, string montadora, string marca, int ano, double potencia)
+    {
+        this.modelo = modelo;
+        this.montadora = montadora;
+        this.marca = marca;
+        this.ano = ano;
+        this.potencia = potencia;
+    }
+}
