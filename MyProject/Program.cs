e33ee27117d03de74213f1e708ab79d4edc505ac
wendel filipe <wendelfilipe1997@hotmail.com>
2023-11-25 15:26:24 -0300

    Learning arrays

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("Exericiso de array");
+
+int[] numeros;
+
+numeros = new int[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+string[] nomes = {"ana", "matheus", "duda", "kaka", "wendel"};
+
+Console.WriteLine(nomes[0]);
