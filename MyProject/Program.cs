48cc24de0ee015c8b492e30966c9e27a09f6e52c
wendel filipe <wendelfilipe1997@hotmail.com>
2023-11-27 09:35:07 -0300

    learning overloading

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+using System.Reflection;
+
+Console.WriteLine("Assinatura de metodo - Sobrecarga");
+
+Email email = new();
+
+email.Enviar("wendel@hotmal", "email");
+
+public class Email
+{
+    public void Enviar(string endereço)
+    {
+        Console.WriteLine($"{endereço}");
+        Console.WriteLine("Assunto padrão");
+    }
+    public void Enviar(string endereço, string assunto)
+    {
+        Console.WriteLine($"{endereço}");
+        Console.WriteLine($"Assunto padrão : {assunto}");
+    }
+    public void Enviar(string endereço, decimal valor)
+    {
+        Console.WriteLine($"{endereço}");
+        Console.WriteLine("proposta comercial");
+        Console.WriteLine($"{valor}");
+    }
+    public void Enviar(decimal valor, string endereço)
+    {
+        Console.WriteLine($"{endereço}");
+        Console.WriteLine("pagamento fornecedor");
+        Console.WriteLine($"{valor}");
+    }
+}
