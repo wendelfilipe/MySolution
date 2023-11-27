e93f63eef6fe560aa16f292c478dff62fa4243e1
wendel filipe <wendelfilipe1997@hotmail.com>
2023-11-27 09:11:21 -0300

    learning arrays-II

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("");
+Console.WriteLine("Array com lop for");
+
+int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+string[] names = { "ana", "kaka", "wendel" };
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Elemento de índice {i} : {numeros[i]}");
+}
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine($"Elemento de índice {i} : {names[i]}");
+}
